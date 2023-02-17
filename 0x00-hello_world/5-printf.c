ba7006b8152e180e114df46346610f96bfb151cb
Motolani Mustapha <mustaphatolani42@gmail.com>
2023-02-17 02:50:02 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this writes out the input
+ * Retun: 0 when successful
+ */
+
+int main(void)
+{
+	 printf("with proper grammar, but the outcome is a piece of art,");
+	 return (0);
+}
