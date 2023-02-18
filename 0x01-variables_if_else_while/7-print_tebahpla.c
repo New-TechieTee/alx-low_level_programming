2abbf596143ad1ddb6ba96b434bb92ccd29c6e41
Motolani Mustapha <mustaphatolani42@gmail.com>
2023-02-18 03:24:44 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- print function
+ *
+ * Return: 0 when necessary
+ */
+
+int main(void)
+{
+char i;
+for (i = 'z' ; i >= 'a' ; i--)
+	putchar(i);
+putchar('\n');
+return (0);
+}
