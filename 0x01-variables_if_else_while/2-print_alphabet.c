377aec73d680377accfb85480611fc30161d34cd
Motolani Mustapha <mustaphatolani42@gmail.com>
2023-02-18 01:40:26 -0800

    for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - run function
+ *
+ * Return: 0 when needed
+ */
+
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+putchar('\n');
+return (0);
+}
