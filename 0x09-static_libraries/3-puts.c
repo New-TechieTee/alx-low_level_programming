251586e15adbf90974316df9856eb1ae670f707a
Motolani Mustapha <mustaphatolani42@gmail.com>
2023-03-18 12:30:01 -0700

    static libraries are fun

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void _puts(char *str)
+
+#include<stdio.h>
+/**
+ * main - Print function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
