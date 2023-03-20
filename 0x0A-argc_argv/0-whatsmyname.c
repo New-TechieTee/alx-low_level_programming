61f5297092a097d309c08ae5d1ae024661294550
Motolani Mustapha <mustaphatolani42@gmail.com>
2023-03-20 16:46:13 -0700

    start again

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,9 @@
  * Return: 0 when necessary
  */
 
-int main(int _attribute_((unused))argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void) argc;
+	if (argc > 0)
 	printf("%s\n", argv[0]);
 	return (0);
 }
