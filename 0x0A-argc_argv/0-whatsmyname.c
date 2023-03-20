285428e870a6a3ea77a0e2dbe1616f606f43092c
Motolani Mustapha <mustaphatolani42@gmail.com>
2023-03-20 16:38:38 -0700

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main: Entry point
+ *
+ * @argc: counts the number of command line arguments in main
+ * @argv: points to the array of parameters that go into main
+ *
+ * Return: 0 when necessary
+ */
 
 int main(int _attribute_((unused))argc, char *argv[])
 {
