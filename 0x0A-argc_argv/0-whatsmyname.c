7cd6085dd302b2821b4f7f4e8c622ae557f21a02
Motolani Mustapha <mustaphatolani42@gmail.com>
2023-03-21 15:27:52 -0700

    0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main: Entry point
+ * main -  Entry point
  *
  * @argc: counts the number of command line arguments in main
  * @argv: points to the array of parameters that go into main
