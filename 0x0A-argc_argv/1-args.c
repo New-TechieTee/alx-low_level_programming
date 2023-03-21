bd6891bd783bda4816f7ae86391405809b2ed1c7
Motolani Mustapha <mustaphatolani42@gmail.com>
2023-03-21 15:34:07 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - start from here
+ *
+ * @argc: counts number of command line parameters
+ * @argv: pointer of array of strings used in command line
+ *
+ * Return: 0 when succesful
+ */
+
+int main(int argc, char **argv)
 {
 	(void) argv;
 	printf("%d\n", argc - 1);
