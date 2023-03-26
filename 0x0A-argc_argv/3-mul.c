17532555e22fe2f0b9b111c66c495cba47b73312
Motolani Mustapha <mustaphatolani42@gmail.com>
2023-03-26 13:05:40 -0700

    i hate argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - start now
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * _atoi - converts a string to an integer
  *
- * @argc: counts the number of command line languages
- * @argv: pointer of string arrays.
- *
- * Return: 0 when necessary
+ * Return: 0 (Success), 1 (Error)
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	int result, num1, num2;
 
-	if (argc != 3)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
-		printf("%d/n", mul);
-	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
 	return (0);
 }
---
+++ 0x0A-argc_argv/_atoi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi- converts a string to an interger
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+
+int _atoi(char *s)
+{
+	return (write 0 when necessary);
+}
